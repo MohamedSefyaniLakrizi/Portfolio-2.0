7074393f0ad4d1e22dc6a590bb2db225c23690d2
mohamed <mohamedsefyani@gmail.com>
2024-08-20 15:52:03 +0100

    changed images and videos urls

--- components/Projects.vue
+++ components/Projects.vue
@@ -2,25 +2,25 @@
   <div class="grid grid-cols-1 md:grid-cols-2 w-full gap-10">
     <ProjectItem
       link="/dari-immobilier"
-      image="/_nuxt/assets/images/dari-immobilier.png"
+      image="https://x0u1tagseemx97ht.public.blob.vercel-storage.com/images/dari-immobilier-mkiD4euEFS6uy5i41BAhaYhCRQH04t.png"
       title="Real Estate Listings Website"
       description="Nuxt, Vue, Tailwind CSS, Supabase"
       alt="dari immobilier" />
     <ProjectItem
       link="/airbnb-clone"
-      image="/_nuxt/assets/images/airbnb-clone.png"
+      image="https://x0u1tagseemx97ht.public.blob.vercel-storage.com/images/airbnb-clone-d7iCLLgGPJY7DfANXrsKfld80IOzBj.png "
       title="Airbnb Clone"
       description="React, Tailwind CSS"
       alt="airbnb clone" />
     <ProjectItem
       link="/fairstly"
-      image="/_nuxt/assets/images/fairstly.png"
+      image="https://x0u1tagseemx97ht.public.blob.vercel-storage.com/images/fairstly-46esrdHO6OSbQcKjwLowKpk6GgPJ0K.png"
       title="Shopify Theme"
       description="JS, Tailwind CSS, JQuery, Liquid (Shopify)"
       alt="fairstly" />
     <ProjectItem
       link="/therapist-website"
-      image="/_nuxt/assets/images/Therapist-website.png"
+      image="https://x0u1tagseemx97ht.public.blob.vercel-storage.com/images/Therapist-website-DxIQXfzspuw6VxYiPU58T7VykAy1Ub.png"
       title="Website for therapist appointments"
       description="Vue, Tailwind CSS"
       alt="therapist website" />
