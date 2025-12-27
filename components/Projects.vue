5fbe52e054151390443b63c57803500e901ca22e
Mohamed Sefyani Lakrizi <102101395+MohamedSefyaniLakrizi@users.noreply.github.com>
2025-12-27 14:29:51 +0000

    Update project details in Projects.vue

--- components/Projects.vue
+++ components/Projects.vue
@@ -1,22 +1,22 @@
 <template>
   <div class="grid grid-cols-1 md:grid-cols-2 w-full gap-10">
     <ProjectItem
-      link="/dari-immobilier"
-      image="https://x0u1tagseemx97ht.public.blob.vercel-storage.com/images/dari-immobilier-mkiD4euEFS6uy5i41BAhaYhCRQH04t.png"
+      link="/nwi"
+      image="https://x0u1tagseemx97ht.public.blob.vercel-storage.com/images/nwi.ma/Home%20NWI.ma.png"
       title="Real Estate Listings Website"
-      description="Nuxt, Vue, Tailwind CSS, Supabase"
-      alt="dari immobilier" />
+      description="Nuxt, Tailwind, PostgreSQL"
+      alt="nwi" />
     <ProjectItem
       link="/airbnb-clone"
       image="https://x0u1tagseemx97ht.public.blob.vercel-storage.com/images/airbnb-clone-d7iCLLgGPJY7DfANXrsKfld80IOzBj.png "
       title="Airbnb Clone"
-      description="React, Tailwind CSS"
+      description="React, Tailwind"
       alt="airbnb clone" />
     <ProjectItem
       link="/fairstly"
       image="https://x0u1tagseemx97ht.public.blob.vercel-storage.com/images/fairstly-46esrdHO6OSbQcKjwLowKpk6GgPJ0K.png"
       title="Shopify Theme"
-      description="JS, Tailwind CSS, JQuery, Liquid (Shopify)"
+      description="JQuery, Tailwind, Liquid (Shopify)"
       alt="fairstly" />
     <ProjectItem
       link="/therapist-website"
