78655d478d4c69a509105f5fad9c5ba3a583c850
Mohamed Sefyani Lakrizi <102101395+MohamedSefyaniLakrizi@users.noreply.github.com>
2026-01-08 13:37:22 +0000

    Create Axiom Labs project details component
    Added a new project details component for a client management SaaS.

---
+++ pages/axiom labs.vue
@@ -0,0 +1,25 @@
+<template>
+  <ProjectDetails
+    title="Client Management SaaS"
+    involvement="Design, Development"
+    technologies="Next, Tailwind, TypeScript, PostgreSQL"
+    video="https://x0u1tagseemx97ht.public.blob.vercel-storage.com/videos/Recording%202026-01-08%20132447%20compressed.mp4"
+    alt_video="Overview of the platform"
+    image1="https://x0u1tagseemx97ht.public.blob.vercel-storage.com/images/client%20management/dashboard.png"
+    alt_image1="Dashboard Page"
+    image2="https://x0u1tagseemx97ht.public.blob.vercel-storage.com/images/client%20management/Calendar.png"
+    alt_image2="Calendar Page"
+    image3="https://x0u1tagseemx97ht.public.blob.vercel-storage.com/images/client%20management/client%20page.png"
+    alt_image3="Client Page"
+    image4="https://x0u1tagseemx97ht.public.blob.vercel-storage.com/images/client%20management/Meeting.png"
+    alt_image4="Meeting Page"
+    image5="https://x0u1tagseemx97ht.public.blob.vercel-storage.com/images/client%20management/Invoice.png"
+    alt_image5="Invoice Page"
+    image6="https://x0u1tagseemx97ht.public.blob.vercel-storage.com/images/client%20management/Note%20Editor.png"
+    alt_image6="Note Editor"
+    description="I developed a client management website designed for psychologist, the platform handles clients, appointments, notes, invoices, availability, and meetings. The website also has a client focused landing page with a form that has availability directly linked to the platform"
+    website="https://dashboard-appointment-website.vercel.app/"
+    nogithub="P.S: The website is accessible but authorization is restricted to clients" />
+</template>
+
+<script setup lang="ts"></script>
